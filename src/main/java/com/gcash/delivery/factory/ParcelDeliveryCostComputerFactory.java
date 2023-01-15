ef2e83ce4b260c796449c957178c00fd9af19a5e
ivanjustyn-dev <ivanjustyn.dev@gmail.com>
2023-01-15 23:21:13 +0800

    update

--- src/main/java/com/gcash/delivery/factory/ParcelDeliveryCostComputerFactory.java
+++ src/main/java/com/gcash/delivery/factory/ParcelDeliveryCostComputerFactory.java
@@ -4,12 +4,9 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import com.gcash.delivery.model.Parcel;
-import com.gcash.delivery.model.ParcelDeliveryCost;
-import com.gcash.delivery.model.Rate;
 import com.gcash.delivery.util.IParcelDeliveryCostComputerUtil;
 import com.gcash.delivery.util.VolumeParcelDeliveryComputerUtil;
 import com.gcash.delivery.util.WeightParcelDeliveryCostComputerUtil;
-import com.gcash.delivery.voucher.model.Discount;
 
 @Component
 @Primary
